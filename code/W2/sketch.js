bea619f656d619146dea60bf54b34ca0cf9059ec
Eugene Ahn <eugeneak1210.gmail.com>
2025-01-23 20:12:09 -0500

    button event

--- code/W2/sketch.js
+++ code/W2/sketch.js
@@ -1,12 +1,22 @@
+let btn;
+
 function setup() {
-    createCanvas(400, 400);
-    rectMode(CENTER);
-  }
+  createCanvas(400, 400);
+  rectMode(CENTER);
+
+  btn = select('#my-button');
+  btn.mouseClicked(onBtnClicked);
+
+}
   
-  function draw() {
-    background(220);
-    circle(100, 100, 100);
-    rect(100, 100, 100);
-    circle(40,40, 40);
-    rect(200, 100, 100);
-  }
+function onBtnClicked() {
+  fill(random(0, 255));
+}
+
+function draw() {
+  background(220);
+  circle(100, 100, 100);
+  rect(100, 100, 100);
+  circle(40,40, 40);
+  rect(200, 100, 100);
+}
