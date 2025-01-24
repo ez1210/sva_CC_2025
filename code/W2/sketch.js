341b3bc97c93802d45c5488a2179460952c4ce28
Eugene Ahn <eugeneak1210.gmail.com>
2025-01-23 19:38:46 -0500

    html changed

---
+++ code/W2/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400, 400);
+    rectMode(CENTER);
+  }
+  
+  function draw() {
+    background(220);
+    circle(100, 100, 100);
+    rect(100, 100, 100);
+    circle(40,40, 40);
+    rect(200, 100, 100);
+  }
