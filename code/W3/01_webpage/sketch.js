9d4897eeb257d3c1c134c863aa98bd08db9cfe88
Eugene Ahn <eugeneak1210.gmail.com>
2025-01-30 20:10:58 -0500

    multiple events craeated

--- code/W3/01_webpage/sketch.js
+++ code/W3/01_webpage/sketch.js
@@ -1,5 +1,41 @@
+let links = [];
+
+
 function setup() {
-    createCanvas(500, 500);
+
+    
+    createCanvas(200, 200);
     background(190);
-    ellipse(100, 100, 100);
+    
+    for (let i = 0; i< 25; i++) {
+        const link = createA("#", "Yahoo!");
+        link.position (random(500), random(500));
+        link.mouseClicked(onMouseClick);
+        link.mouseOut(onMouseOut);
+        link.mouseOver(onMouseOver);
+        links.push(link);
+    }
+
+    //createP("this is from p5js");
+
+    const p = createP("this is from p5js");
+    p.position(100, 100);
+
+    
+
+}
+
+function onMouseClick() {
+    this.style("background-color", "green");
+    this.style("color", "white");
+}
+
+function onMouseOver() {
+    this.style("background-color", "pink");
+    this.style("color", "black");
+}
+
+function onMouseOut() {
+    this.style("background-color", "red");
+    this.style("color", "white");
 }
