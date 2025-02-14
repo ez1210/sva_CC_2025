0abfb84a3e9ca772b28875bbebc815240ecb6d4f
Eugene Ahn <eugeneak1210.gmail.com>
2025-02-13 21:12:13 -0500

    W5 particle systems

---
+++ code/W5/particle_system/sketch.js
@@ -0,0 +1,15 @@
+let particleSystem;
+
+function setup() {
+    createCanvas(800, 400);
+    particleSystem = new ParticleSystem();
+}
+
+function draw() {
+    background(190);
+    particleSystem.loop();
+}
+
+function mousePressed() {
+    particleSystem.addParticles(mouseX, mouseY, 10);
+}
