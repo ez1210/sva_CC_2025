f87cde48363d6387bb30a06933838258089d51e8
Eugene Ahn <eugeneak1210.gmail.com>
2025-03-27 21:18:25 -0400

    w10 added

---
+++ code/W10/promise/sketch.js
@@ -0,0 +1,69 @@
+let circleX, circleY, circleSize, circleColor;
+let message = "Nothing!";
+
+function setup() {
+    createCanvas(500, 300);
+    console.log('start:', millis());
+    setTimeout(callBack, 5000);
+    circleX = width/2;
+    circleY = height/2;
+    circleSize = 50;
+    circleColor = color(255, 0, 0);
+}
+
+function draw() {
+    background(180);
+    fill(circleColor);
+    circle(circleX, circleY, circleSize);
+    fill(0);
+    text (message, width * 0.45, height * 0.1);
+}
+
+function mouseClicked() {
+    console.log('click');
+    anim();
+}
+
+function delay(timeDelay) {
+    return new Promise ( (resolve) => setTimeout(resolve, timeDelay));
+    //resolve => setTimeout(resolve, timeDelay);
+};
+
+async function anim() {
+    await delay(1000);
+    circleSize = 150;
+
+    await delay(1000);
+    circleX = width * 0.75;
+    message = 'move to right';
+
+    await delay(1000);
+    circleColor = color(0, 255, 0);
+
+    await delay(1000);
+    circleY = height * 0.75;
+
+    await delay(1000);
+    circleSize = 50;
+
+    setTimeout(() => {
+        circleSize = 150;
+        setTimeout(() => {
+            circleX = width * 0.75;
+            setTimeout(() => {
+                circleColor = color(0, 255, 0);
+                setTimeout(() => {
+                    circleX = width * 0.5;
+                    setTimeout(() => {
+                        circleSize = 50;
+                    }, 1000)
+                }, 1000)
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}
+
+function callBack() {
+    console.log('time out:', millis());
+}
+
